436cc5538f82533bffaaaff24668e8a888bee4aa
Leak <sethpowellbusiness@gmail.com>
2021-08-27 11:25:13 -0700

    built out hide component

--- src/components/hide.js
+++ src/components/hide.js
@@ -6,14 +6,41 @@ export default class Hide extends Component {
 
 		this.state = {
 			hidden: false,
+			style: {
+				display: "flex",
+			},
 		};
+
+		this.toggleDisplay = this.toggleDisplay.bind(this);
+	}
+
+	toggleDisplay() {
+		if (!this.state.hidden) {
+			this.setState({
+				hidden: true,
+				style: {
+					display: "none",
+				},
+			});
+		} else if (this.state.hidden) {
+			this.setState({
+				hidden: false,
+				style: {
+					display: "flex",
+				},
+			});
+		}
 	}
 
 	render() {
 		return (
 			<div className="hide-wrapper">
-				<h1 className="hide">Hide Me!</h1>
-				<button className="hide-btn">Hide</button>
+				<h1 className="hide" style={this.state.style}>
+					Hide Me!
+				</h1>
+				<button className="hide-btn" onClick={this.toggleDisplay}>
+					Hide
+				</button>
 			</div>
 		);
 	}
