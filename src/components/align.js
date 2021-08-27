b5a2c32722d21ade966c9b4ac31f77a7aaff30d7
Leak <sethpowellbusiness@gmail.com>
2021-08-27 11:12:25 -0700

    built out align component

--- src/components/align.js
+++ src/components/align.js
@@ -5,17 +5,59 @@ export default class Align extends Component {
 		super();
 
 		this.state = {
-			style: null,
+			style: {
+				display: "flex",
+				justifyContent: "flex-start",
+			},
 		};
+
+		this.handleLeftClick = this.handleLeftClick.bind(this);
+		this.handleRightClick = this.handleRightClick.bind(this);
+		this.handleCenterClick = this.handleCenterClick.bind(this);
+	}
+
+	handleLeftClick() {
+		this.setState({
+			style: {
+				display: "flex",
+				justifyContent: "flex-start",
+			},
+		});
+	}
+
+	handleRightClick() {
+		this.setState({
+			style: {
+				display: "flex",
+				justifyContent: "flex-end",
+			},
+		});
+	}
+
+	handleCenterClick() {
+		this.setState({
+			style: {
+				display: "flex",
+				justifyContent: "space-around",
+			},
+		});
 	}
 
 	render() {
 		return (
 			<div className="align-wrapper">
-				<h1 className="align-text">Align Me!</h1>
-				<button className="left-btn">Left</button>
-				<button className="center-btn">Center</button>
-				<button className="right-btn">Right</button>
+				<div className="flex-text-wrapper" style={this.state.style}>
+					<h1 className="align-text">Align Me!</h1>
+				</div>
+				<button className="left-btn" onClick={this.handleLeftClick}>
+					Left
+				</button>
+				<button className="center-btn" onClick={this.handleCenterClick}>
+					Center
+				</button>
+				<button className="right-btn" onClick={this.handleRightClick}>
+					Right
+				</button>
 			</div>
 		);
 	}
