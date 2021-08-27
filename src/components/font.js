267b477de5068c1e71509dbddc50ac7333ef4e2a
Leak <sethpowellbusiness@gmail.com>
2021-08-27 12:00:07 -0700

    built out font component buttons

--- src/components/font.js
+++ src/components/font.js
@@ -6,19 +6,53 @@ export default class Font extends Component {
 
 		this.state = {
 			size: 12,
+			style: {
+				fontSize: "12px",
+			},
 		};
+
+		this.fontInc = this.fontInc.bind(this);
+		this.fontDec = this.fontDec.bind(this);
+	}
+
+	fontInc() {
+		const sizeUp = this.state.size + 1;
+		const upFontSize = `${sizeUp}px`;
+		this.setState({
+			size: sizeUp,
+			style: {
+				fontSize: upFontSize,
+			},
+		});
+	}
+
+	fontDec() {
+		const sizeDown = this.state.size - 1;
+		const downFontSize = `${sizeDown}px`;
+		this.setState({
+			size: sizeDown,
+			style: {
+				fontSize: downFontSize,
+			},
+		});
 	}
 
 	render() {
 		return (
 			<div className="font-wrapper">
-				<h1 className="font-number">{this.state.size}px</h1>
+				<h1 className="font-number" style={this.state.style}>
+					{this.state.size}px
+				</h1>
 				<input
 					className="font-input"
 					placeholder={this.state.size}
 				></input>
-				<button className="font-up-btn">Increase</button>
-				<button className="font-down-btn">Decrease</button>
+				<button className="font-up-btn" onClick={this.fontInc}>
+					Increase
+				</button>
+				<button className="font-down-btn" onClick={this.fontDec}>
+					Decrease
+				</button>
 			</div>
 		);
 	}
